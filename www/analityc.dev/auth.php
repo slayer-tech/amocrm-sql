136f846f80aa246f33fcd920ef2bb6d75c3233d3
slayer <51415821+slayer-tech@users.noreply.github.com>
2024-06-24 15:18:09 +0300

    Update auth.php

--- www/analityc.dev/auth.php
+++ www/analityc.dev/auth.php
@@ -4,16 +4,16 @@
 include_once __DIR__ . '/token_actions.php';
 
 
-$subdomain = 'makhota'; //Поддомен нужного аккаунта
+$subdomain = ''; //Поддомен нужного аккаунта
 $link = 'https://' . $subdomain . '.amocrm.ru/oauth2/access_token'; //Формируем URL для запроса
 
 /** Соберем данные для запроса */
 $data = [
-    'client_id' => '27118436-e36e-42d5-ac9a-35ddebb3b57d',
-    'client_secret' => '9e4ub8AytgrTgbfz4FLsR0lvNeCB1TSiYF6TjwgTrVUMLSnEHh7wQfMVY7vPt3IL',
+    'client_id' => '',
+    'client_secret' => '',
     'grant_type' => 'authorization_code',
-    'code' => 'def502003064e1f0f3a9b602bb64df8845cf1f8e328f933a2c3f57a9d0349e8acd9036d6f7eda1dfa3aa249738734acc09bdc44bb3632ccf33ce8cedce40d5664cf4a522cce22e870d45cd61427271b765c069878d83cf016a23c2fa0197b56240b374e4fc4efa1613d1d41e7d498d7b2c2b9d84f3442ee39b98264f61e6822a6fc819a5ae49f3e199312fc3c921713a282c718afeee8c2b4772b14dc86c1f67e65e7898ea41ca08a15889e311f473e08bf7c728b2325f80c8fdda59b94615e72e80e2fbf5ad9a979bce0e414261532a7e8e934341448d547d8297b6c2f29f7bd269084d70c103db7d2f607fd218389d182f16af637b8752362f548693fe712a17f17ef56085e308f6d58e0eee50ea67db1a721cce33ffdb41a3acc956e3fad9b4a0554eb8af89edd375d624b696534792b1f899e34acd8af1c504dfd650aedcff5de2504112ab7d630bdd5016b7d25a5422c57f4ae7fb837c3c19f44879b8391451e1397185d50b911fb9fc06b346f91baa42a9bf6ae72f23fffa34dfe4ff938905540fee5980e3a61c7af582ec6f36ee1045b16cde7299e7fa8a9a56726cae4448d72bf1e14f877f10151bbf57d6f3256e308b13fb264eb182e1fe1f6c5c2d9ba28d91a244b034ab817795c5aec1268565af97',
-    'redirect_uri' => 'https://apps.telecombg.ru/',
+    'code' => '',
+    'redirect_uri' => '',
 ];
 
 /**
@@ -77,5 +77,5 @@ saveToken([
     'accessToken' => $access_token,
     'refreshToken' => $refresh_token,
     'expires' => $expires_in,
-    'baseDomain' => 'makhota.amocrm.ru'
+    'baseDomain' => ''
 ]);
